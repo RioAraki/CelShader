3913a520a49d3645beb17bba1405e42188f8d35a
RioAraki <yueee.li@mail.utoronto.ca>
2018-12-03 01:18:35 -0500

    add source explicitly

---
+++ TestShader/Assets/lightChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightChange : MonoBehaviour {
+    // Use this for initialization
+    public float start;
+    void Start () {
+        start = Time.realtimeSinceStartup;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.rotation = Quaternion.Euler(transform.rotation.x + Time.realtimeSinceStartup * 30 - start, transform.rotation.y + Time.realtimeSinceStartup * 30 - start, transform.rotation.z);
+    }
+}
